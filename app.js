6c9292fa5fd8134c2e41c49b4b5cc59db2533d48
rxdri <rxdrii@proton.me>
2023-03-09 20:43:16 +0100

    some changes

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+let form = document.getElementById("form")
+let user = document.getElementById("user")
+let email = document.getElementById("email")
+let pasword = document.getElementById("pasword")
+let pasword2 = document.getElementById("confirmpws")
+let button = document.getElementById("button")
 
+
+function verifyUser(user){
+    let length = user.length
+    if(length < 5){
+        document.getElementById("user").style.border = "red"
+    }else if(length > 25){
+        document.getElementById("user").style.border = "red"
+    }
+}
+function submit(){
+    verifyUser
+}
+
+button.addEventListener("submit",submit)
